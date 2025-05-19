588a5036e7b5ea39063e370e7c6881f01830c498
josephjohny4321 <josephjohny4321@gmail.com>
2025-05-19 10:03:49 +0000

    trying mongo

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,21 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
+const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
 
 app.use(cors({origin: 'https://localhost:5173/'}));
 app.use(express.json());
 
+let selection;
+
 app.get('/', (req, res) => {
-  res.send('get');
+  res.send('get from chess');
 });
 
-app.post('/', (req, res) => {
-  res.send('post');
+app.post('/selection', (req, res) => {
+  selection = req.body.selection;
+  res.send(('post from chess with selection: ' + selection));
 });
 
 app.listen(port, () => {
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import axios from 'axios'
 
 
 function App() {
-  const token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL2Nsb3VkLmdvb2dsZS5jb20vd29ya3N0YXRpb25zIiwiYXVkIjoiaWR4LWNoZXNzLTE3NDY0MzUwNjc0ODQuY2x1c3Rlci02ZnJuaWk0M281YmxjdTUyMnNpdmVienBpaS5jbG91ZHdvcmtzdGF0aW9ucy5kZXYiLCJpYXQiOjE3NDY2ODM2NzksImV4cCI6MTc0Njc3MDA3OX0.Xy5lSpPAIah5QHT2jlAX4IXBb2In7gyAK1ray80x71zM8IfmbA41g5GjRKAkePhcpvhV2LWaxcrENj4Oot42bRILmYsK8uqKDZQ6oAnAVZsoKcuZnYwVmkhnq6DEtrbZsknuaNgmTwJruNSXI7a5BkYTaqkQs4gvfy5ybCN6PFOwxpJccHw-enHDVIGUu0bkJD_Rg7FevLd6AesljIgkkh7KSpulAGhlDDHzjuNoBx8P7OdC2PhtDo_Y_AKWirCPQQw-1h1PSPBN8Q4d7LQXS3g9LmcUG51KeKOxZM6wbwRRQa_BpSWgCwHKmRdyVHmdle0QiaCh8hTUQU6PqRbP0w';
+  const token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL2Nsb3VkLmdvb2dsZS5jb20vd29ya3N0YXRpb25zIiwiYXVkIjoiaWR4LWNoZXNzLTE3NDY0MzUwNjc0ODQuY2x1c3Rlci02ZnJuaWk0M281YmxjdTUyMnNpdmVienBpaS5jbG91ZHdvcmtzdGF0aW9ucy5kZXYiLCJpYXQiOjE3NDc2NDMyOTgsImV4cCI6MTc0NzcyOTY5OH0.LebyFZtSrpKWU_dBzVDpzQU2dnkWsfCq2waHSsx_sBtGS4uDtVD3yGFY2_JdPLpGiFZS8tO3Iyp1JaVhoHtjF1KWArjhU06xlqVQvdZ8OMTTr22irxyBaJxjbvDViVIFS7Q_J5iNk648cBAoD4X4JqzZ4qsx1Ijnj4-5JufpEcMP0dXwX1Hj3RXjZfkLhJ-NVtAbqNbqbacczbBrta-yH23f0qGedIGbEkwufU9WgjbzYey-fSPcFl5xqt0mgmWJrHLuMYYjnsSXtWPNt3FN3dhwp4B9k4_TdGekx_5MrK1cyOzRQZQsdiI8Wno1WEVahcaoLi48-1vV6wog6u91KA';
   
   const newGameState = {
     black: ['a7','b7','c7','d7','e7','f7','g7','h7','Ra8','Rh8', 'Nb8', 'Ng8', 'Bc8', 'Bf8', 'Qe8', 'Kd8'], 
@@ -28,7 +28,7 @@ function App() {
   }, [])
 
   useEffect(() =>{
-    axios.post('https://localhost:3000/', {
+    axios.post('https://localhost:3000/selection',{selection: selection} ,{
       headers: {
         Authorization: `Bearer ${token}`  
       }
